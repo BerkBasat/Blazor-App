14c8047e805ec84d1c499c25c50c0cbd9940fd9e
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-01-16 12:56:25 +0300

    Project setup, db created.

---
+++ Blazor_App/Blazor_App/Server/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Blazor_App.Server.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor_App.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
